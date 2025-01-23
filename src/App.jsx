cbe721847b92c25e57e07e3fe5a5c8b89fd72184
Fadeela Ali <fadeelaali43@gmail.com>
2025-01-23 03:48:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   // const [varName, setterFunc] = useState(optionalDefaultValue)
   const [todos, setTodos] = useState([
-    {input: 'Hello! Add your first todo', complete: true}
+    {input: 'Hello! Add your first todo', complete: false}
   ])
   const [selectedTab, setSelectedTab] = useState('Open')
   
